9c791e1288ebe22b3437ed5f888aaa48995aadbb
raphael ferreira <raphael.ferreira@workcard.com.br>
2017-07-26 17:33:09 -0300

    server using lib strategy

--- api/server.js
+++ api/server.js
@@ -1,36 +1,21 @@
 // @flow
 /* eslint-disable no-console */
-import "source-map-support/register";
+
 import express from "express";
 import mongoose from "mongoose";
 import morgan from "morgan";
 import bodyParser from "body-parser";
-import jwt from "jsonwebtoken";
 import passport from "passport";
-import BearerStrategy from "passport-http-bearer";
+import bearer from "./lib/strategys";
 import config from "./tools/config";
 import routes from "./routes";
 
 const port: string = config.PORT;
 const app = express();
-const strategy = new BearerStrategy((token, done) => {
-  jwt.verify(token, config.SECRET, (err, user) => {
-    if (err) {
-      return done("err");
-    }
-    if (!user) {
-      return done(null, {
-        success: false,
-        message: "Failed to authenticate token."
-      });
-    }
-    return done(null, user._doc);
-  });
-});
 
 mongoose.connect(config.DB_URL);
 
-passport.use(strategy);
+passport.use(bearer);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
