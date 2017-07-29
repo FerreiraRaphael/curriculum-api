78c342900784ec6fe377e7bb531b0f0e4a85ef8f
Raphael Ferreira <raphaelbsferreira@gmail.com>
2017-07-29 19:57:08 -0300

    created skills controller

---
+++ api/models/Skills/controller.js
@@ -0,0 +1,8 @@
+// @flow
+import HttpStatus from "http-status-codes";
+import Model from "./facade";
+import Controller from "../../lib/controller";
+
+class SkillController extends Controller {}
+
+export default new SkillController(Model);
