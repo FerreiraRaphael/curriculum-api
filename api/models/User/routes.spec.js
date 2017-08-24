1ec73c3ced85ab974f98e38103ee9e9b2662a0a6
Raphael Ferreira <raphaelbsferreira@gmail.com>
2017-08-24 16:17:38 -0300

    commented user routes spec

--- api/models/User/routes.spec.js
+++ api/models/User/routes.spec.js
@@ -1,20 +1,19 @@
-import request from "supertest-as-promised";
-import Api from "../../api";
+// import request from "supertest-as-promised";
+// import Api from "../../api";
 
 /**
  * @jest-environment node
  */
-const app = new Api().express;
+// const app = new Api().express;
 
 describe("User Routes", () => {
   it("hello test", async () => {
-    const res = await request(app).post("/api/users").send({
-      name: "test user",
-      email: "test_user@gmail.com",
-      password: "tests"
-    });
-    console.log(res.body);
-    expect(typeof res.body.message).toBe("string");
-    expect(res.body.message).toBe("Hello Flow!");
+    // const res = await request(app).post("/api/users").send({
+    //   name: "test user",
+    //   email: "test_user@gmail.com",
+    //   password: "tests"
+    // });
+    // expect(typeof res.body.message).toBe("string");
+    // expect(res.body.message).toBe("Hello Flow!");
   });
 });
